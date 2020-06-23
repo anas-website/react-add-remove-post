e4d9bfc672e67b279eaef453c78c6ac20cc93557
anas <anas3506606@gmail.com>
2020-06-23 18:22:59 +0300

    Updates

---
+++ precache-manifest.7dafe6145070f36272f5534d62213239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415fb1a4082dbc543883e919bf0f24ea",
+    "url": "/react-add-remove-post/index.html"
+  },
+  {
+    "revision": "d61f3e1882247345da1c",
+    "url": "/react-add-remove-post/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "cab351dae48822fda097",
+    "url": "/react-add-remove-post/static/js/2.b0e3a124.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-add-remove-post/static/js/2.b0e3a124.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61f3e1882247345da1c",
+    "url": "/react-add-remove-post/static/js/main.d49ef286.chunk.js"
+  },
+  {
+    "revision": "ca29b3c19e09c62401a5",
+    "url": "/react-add-remove-post/static/js/runtime-main.6e80740f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-add-remove-post/precache-manifest.7dafe6145070f36272f5534d62213239.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-add-remove-post/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
